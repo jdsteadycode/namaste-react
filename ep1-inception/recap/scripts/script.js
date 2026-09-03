0b9580d96c51c6d803676d10188f1fb623ccca71
jdsteadycode <desaijeet11@gmail.com>
2026-09-03 10:39:55 +0530

    JavaScript file added

---
+++ ep1-inception/recap/scripts/script.js
@@ -0,0 +1,26 @@
+// check log**
+// console.log("Script loaded!");
+
+/* Way 1 - 
+// grab the root element (`section`)
+const rootEl = document.querySelector("#root");
+// console.log(rootEl);
+
+// a new h1 element.
+const headingEl = document.createElement("h1");
+
+// set content for it.
+headingEl.innerText = "Hello World";
+
+// append heading element as child for the `root` element!
+rootEl.appendChild(headingEl);
+*/
+
+/* Way 2 -
+// grab the root element (`section`)
+const rootEl = document.querySelector("#root");
+
+// update its inner HTML with heading containing Hello World!
+rootEl.innerHTML = "<h1>Hello World</h1>";
+*/
+
